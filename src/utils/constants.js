1d3a764ddf9f6e3f2b04e7a43530fc71eab7e61b
TLong_1512 <125524778+ThanhLong1512@users.noreply.github.com>
2025-08-02 15:56:06 +0700

    deploy lần thứ n+1

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const API_ROOT = "http://localhost:8080";
+export const SOCKET_URL = "http://localhost:8090";
+export const ADMIN_ID = "683ff28614a8b4f3f039ab98";
+export const GOOGLE_MAP_API_KEY = "AIzaSyBGlNOwLui5QUG9PpI2XNq2lv9WYTmcNrA";
+export const DOMAIN_AUTH0 = "dev-xf3ywtzpm74cjt4g.us.auth0.com";
+export const CLIENT_ID_AUTH0 = "MEkIkCTykxjIC62P7Jrc7Tm68aWccf0F";
+export const REACT_GOOGLE_CLIENT_ID =
+  "535392838508-k6900k99k4oatm4208oc9dfgoqcfmu36.apps.googleusercontent.com";
+export const PAGE_SIZE = 10;
